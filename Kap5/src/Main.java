ba40224a8862d0bc932363e458abdf9338b348be
Mezzeto <Mathias_Lundby@hotmail.com>
2018-10-17 14:08:21 +0200

    xml still there

--- Kap5/src/Main.java
+++ Kap5/src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("kaker");
         sut();
-        System.out.println("Endret  ");
     }
 
     public static void sut() {
